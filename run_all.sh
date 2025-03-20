0f1762d686399151056f50d2cb1c3d989dfdf2a8
Brock <kbrock@ab.mpg.de>
2025-03-20 21:24:57 +0100

    fix runall

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,5 @@
 docker build . -t juli-sliprs
 
 docker run juli-sliprs julia_first.R 2> julia_first.log
-docker run juli-sliprs terra_first.R 2> julia_first.log
+docker run juli-sliprs terra_first.R 2> terra_first.log
 docker run juli-sliprs magic.R > magic.log 2>&1 
