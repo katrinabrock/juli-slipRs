ec96f49029bd79f54ae9b04adae4e4ab842a2e69
Brock <kbrock@ab.mpg.de>
2025-03-20 18:21:36 +0100

    Delete leftover interactive commands

--- run_all.sh
+++ run_all.sh
@@ -3,7 +3,3 @@ docker build . -t juli-sliprs
 docker run juli-sliprs julia_first.R 2> julia_first.log
 docker run juli-sliprs terra_first.R 2> julia_first.log
 docker run juli-sliprs magic.R > magic.log 2>&1 
-
-docker run juli-sliprs julia_first.R
-docker run juli-sliprs terra_first.R
-docker run juli-sliprs magic.R
